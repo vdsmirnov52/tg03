b8ca28c23902b390a74fd8c275a022886c8b9ed1
Vadim Smirnov <vdsmitnov52@gmail.com>
2019-11-07 16:25:59 +0300

    smirnov@WWW_WDB ~/MyTests/html/mmap
    	изменено:      index.py
    	изменено:      templates/find_brigs.html
    	изменено:      templates/index.html

--- index.py
+++ index.py
@@ -26,18 +26,31 @@ app.config.update(dict(
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
-def connect_pg (ddd = 'host=localhost dbname=b03 port=5432'):
-	g.dbrec = dbtools.dbtools(ddd)
-	return g.dbrec
-	if db and db.last_error:
+def connect_pg (ddd = 'host=212.193.103.21 dbname=b03 port=5432'):
+	db = dbtools.dbtools(ddd)
+	if db and not db.last_error:
 		print "Ok"
 		return db
 
+from pylib import	parse_res as parse
+
 def	is_person (ulogin, password):
-	flash (u'Отсутствуют login: %s  password: %s' % (ulogin, password))
 	if not (ulogin and password):
-		return	False	#render_template('login.html', error=u'Отсутствую login или password')
-	return	True	#"is_person (%s %s)" % (ulogin, password)
+	#	return	False	#render_template('login.html', error=u'Отсутствую login или password')
+		flash (u'Отсутствуют login: %s  password: %s' % (ulogin, password))
+	else:
+		iddb = connect_pg()
+		if iddb:
+			query = "SELECT u.disp, u.type, u.smena, u.subst, p.name, t.name AS tname FROM usr03 u JOIN person_sp p ON u.cod = p.cod JOIN sp_usrtype t ON u.type = t.cod WHERE login='%s' AND passwd='%s'" % (
+				ulogin, password)
+			user = iddb.get_dict(query)
+			if user:
+				session['user'] = parse.ures(user)
+				g.b03 = iddb
+				return	True
+			else:	flash (u'Отсутствуут пользователь (login: %s  password: %s)' % (ulogin, password))
+		else:	flash (u'Нет доступа к БД (%s)' % app.config['DB_GSMP'])
+	return	False	#"is_person (%s %s)" % (ulogin, password)
 
 @app.route('/set', methods=['POST'])
 def	set_entry():
@@ -60,6 +73,7 @@ def	index ():
 	# return	"<h1>Index t03</h1> %s: %s" % (app.config['DB_GSMP'], connect_pg (app.config['DB_GSMP']))
 	sdate = time.strftime("%d-%m-%Y %T", time.localtime(time.time()))
 	g.sdate = sdate
+
 	if session.get('logged_in'):
 		return render_template('index.html')
 	else:	return render_template('login.html', sdate=g.sdate)
@@ -93,23 +107,42 @@ def	nsi ():
 	error = None
 	return render_template('nsi.html', error=error)
 
+def	pdict (d):
+	for k in d.keys():
+		if d.get(k):	print "[%s: %s]" % (k, d[k]),
+	print "#"*22
+
 @app.route('/ajax', methods=['GET', 'POST'])
 def	ajax_html():
 	if request.method == 'POST':
+		pdict (request.form)
 		# name = request.form['name'];
-		shstat = request.form.get('shstat')
-		if shstat == 'calls':		return "~oo_body| %s %s" % (request.form,  render_template('fcalls_head.html'))
-		if shstat == 'brigs':		return "~oo_body| %s %s" % (dict(request.form),  render_template('find_brigs.html'))
+		shstat = request.form.get('stat')
+		query = "QWERT"
+		if shstat == 'calls':
+			return "~oo_body| %s" % render_template('fcalls_head.html', query="query")
+		if shstat == 'brigs':
+			if not request.form.get('sel_bwfind'):
+				return "~oo_body| %s" % render_template('find_brigs.html', query="query")
+			elif request.form['sel_bwfind'] == 1:
+				query = "SELECT * FROM bnaryd WHERE br_id > 0 ORDER BY number"
+			elif request.form['sel_bwfind'] == 2:
+				query = "SELECT * FROM brignrd WHERE smena = 4 ORDER BY number"
+			elif request.form['sel_bwfind'] == 3:
+				query = "SELECT * FROM brignrd WHERE smena = 1 ORDER BY number"
+			return "~ZZZ| %s" % query	#render_template('fcalls_head.html', query=query)
+	#	if shstat == 'brigs':		return "~oo_body| %s %s" % (dict(request.form),  render_template('find_brigs.html'))
 		return """~oo_body| ZZZ %s """ % dict(request.form)
 	return render_template('ajax.html')
 
 def test ():
 	with app.test_request_context():
+		print "connect_pg", connect_pg()
 		print url_for('index')
 		print url_for('login')
 		print url_for('login', next = 'hello', usr_id = '123')
 
 
 if __name__ == '__main__':
-	#test ()
+#	test ()
 	app.run(host='0.0.0.0')
