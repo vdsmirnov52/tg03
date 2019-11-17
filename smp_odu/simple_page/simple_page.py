59bc575c9b0ecd4bd37d4518e8a4942af4e95b0c
Vadim Smirnov <vdsmitnov52@gmail.com>
2019-11-17 22:37:39 +0300

    smirnov@debian K.Marxa 24
    	новый файл:    smp_odu/simple_page/__init__.py
    	новый файл:    smp_odu/simple_page/simple_page.py

---
+++ smp_odu/simple_page/simple_page.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from flask import Blueprint, render_template, abort
+from jinja2 import TemplateNotFound
+
+simple_page = Blueprint('simple_page', __name__, static_folder='/static',
+                        template_folder='/templates')
+
+@simple_page.route('/', defaults={'page': 'index'})
+@simple_page.route('/<page>')
+def show(page):
+    pname = '%s.html' % page
+    # return "Hello %s !" % pname
+    try:
+        print "ZZZ", pname
+        return render_template(pname)
+    except TemplateNotFound:
+        abort(404)
