9d2e8c29298bc1c290670cb7f1fcc02857db75ef
Vadim Smirnov <vdsmitnov52@gmail.com>
2019-11-18 21:05:42 +0300

    smirnov@debian K.Marxa 24
    	изменено:      smp_odu/simple_page/simple_page.py
    	изменено:      templates/rem.html

--- smp_odu/simple_page/simple_page.py
+++ smp_odu/simple_page/simple_page.py
@@ -3,12 +3,14 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
-simple_page = Blueprint('simple_page', __name__, static_folder='/static',
-                        template_folder='/templates')
-
+simple_page = Blueprint('simple_page', __name__, root_path='/home/smirnov/PycharmProjects/g03', static_folder='static',
+                        template_folder='/home/smirnov/PycharmProjects/g03/templates')
+'''
 @simple_page.route('/')
 def rrr():
+    return render_template('rem.html')
     return "Z"*33
+'''
 
 @simple_page.route('/', defaults={'page': 'index'})
 @simple_page.route('/<page>')
@@ -17,6 +19,6 @@ def show(page):
     # return "Hello %s !" % pname
     try:
         print "ZZZ", pname
-        return render_template(pname)
+        return render_template('rem.html', data=pname)
     except TemplateNotFound:
         abort(404)
