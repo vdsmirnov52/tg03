7e9f96d924039f3c284c416dea37b6c9e7a31e08
Vadim Smirnov <vdsmitnov52@gmail.com>
2019-11-20 18:06:54 +0300

    smirnov@wdb CGI VDS
    	изменено:      smp_odu/simple_page/simple_page.py
    	изменено:      templates/login.html
    	новый файл:    templates/test.html

--- smp_odu/simple_page/simple_page.py
+++ smp_odu/simple_page/simple_page.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint, render_template, abort, url_for, current_app
+from flask import Blueprint, render_template, abort, url_for, current_app, session, redirect, request, flash
 from jinja2 import TemplateNotFound
 
+import time
 
 simple_page = Blueprint('simple_page', __name__, static_folder='../../static',
                         template_folder= '../../templates')    #'/templates')
@@ -22,9 +23,61 @@ def show(page):
     # for k in current_app.config.keys():        print '%33s' % k, app.config.get(k)
     # return "Hello %s !" % pname
     try:
+        if session.get('logged_in'):
+            return render_template('index.html')
+        else:
+            return render_template('login.html', sdate=time.strftime("%d-%m-%Y %T", time.localtime(time.time())))
         templates = '%s/templates' % current_app.config.get('APP_ROOT')
         # simple_page['template_folder'] = templates
         print "ZZZ", pname, templates
         return render_template('rem.html', data=simple_page.root_path)  # pname)
     except TemplateNotFound:
         abort(404)
+
+
+from pylib import dbtools
+from pylib import parse_res
+import json
+
+def	get_user(ulogin, password):
+    if not (login and password):
+        flash(u'Отсутствуют прльзователь login: %s  password: %s' % (ulogin, password))
+    else:
+        idb = dbtools.dbtools('host=212.193.103.21 dbname=b03 port=5432')
+        if idb and idb.last_error:
+            # print perse_res.ures(idb.last_error[:2])
+            flash('%s %s' % (idb.last_error[0], str(idb.last_error[1]).decode('UTF-8')))
+        elif idb:
+            ulogin, password = ['6827', 'p6827']
+            query = """SELECT u.disp, u.type, u.smena, u.subst, p.name, t.name AS tname FROM usr03 u
+                JOIN person_sp p ON u.cod = p.cod JOIN sp_usrtype t ON u.type = t.cod WHERE login='%s' AND passwd='%s'""" % (ulogin, password)
+            flash(query)
+            ruser = idb.get_dict(query)
+            if ruser:
+                session['logged_in'] = True
+                session['user'] = json.loads(json.dumps(ruser))
+                return True
+        else:
+            flash("Not connect: ")
+    return False
+
+@simple_page.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+#	flash(u'Вы вышли из системы')	#'You were logged out')
+    return render_template('login.html', sdate = time.strftime("%d-%m-%Y %T", time.localtime(time.time())))
+	# return redirect(url_for('index'))
+
+
+@simple_page.route('/login', methods = ['GET', 'POST'])
+def login():
+    error = None
+    print "\tLogin:"
+    flash(str(request.form))
+    if request.method == 'POST':
+        flash('POST')
+        if get_user(request.form.get('ulogin'), request.form.get('password')):
+            return render_template('test.html')
+        
+    return render_template('login.html', sdate = time.strftime("%d-%m-%Y %T", time.localtime(time.time())), error = error)
+
